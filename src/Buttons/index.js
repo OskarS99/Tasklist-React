93bb9f43aaaf0266b1169a9511b4c38cad6298b4
Oskar Siry <oskarsiry99@gmail.com>
2023-03-17 16:10:06 +0100

    button component modifiction

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,22 +1,22 @@
 import "./style.css"
 
-const Buttons = ({ tasks, hideDoneTasks }) => {
-    tasks.lenght > 0 && (
-        <div className="buttons">
-            <button className="buttons__style">
-                {hideDoneTasks ? "Pokaż" : "Ukryj"}
-                ukończone
-            </button>
-            <button
-                classNamw="buttons__style"
-                disabled={tasks.every(({ done }) => done)}
-            >
-                Ukończ wszystkie
-            </button>
-        </div>
-    )
-
-
-};
+const Buttons = ({ tasks, hideDoneTasks }) => (
+    <div className="buttons">
+        {tasks.lenght > 0 && (
+            <>
+                <button className="buttons__style">
+                    {hideDoneTasks ? "Pokaż" : "Ukryj"}
+                    ukończone
+                </button>
+                <button
+                    classNamw="buttons__style"
+                    disabled={tasks.every(({ done }) => done)}
+                >
+                    Ukończ wszystkie
+                </button>
+            </>
+        )}
+    </div>
+);
 
 export default Buttons;
