418d93d646cbe1533aa3de685f0e436d4923bfe8
Oskar Siry <oskarsiry99@gmail.com>
2023-03-18 16:15:15 +0100

    ternary operation added to section component

--- src/Section/index.js
+++ src/Section/index.js
@@ -2,7 +2,7 @@ import "./style.css";
 
 const Section = ({ title, body, extraHeaderContent }) => (
   <section className="section">
-    <header className="section__header--buttons">
+    <header className={`${title === "Dodaj nowe zadanie" ? "section__header" : "section__header--buttons"}`}>
       <h2 className="section__title">{title}</h2>
       {extraHeaderContent}
     </header>
