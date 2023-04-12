db316eb8007c78051eaa55ff4437eb5f215e8b18
Oskar Siry <oskarsiry99@gmail.com>
2023-04-12 09:30:13 +0200

    code to avoid empty task added

--- src/Form/index.js
+++ src/Form/index.js
@@ -2,34 +2,29 @@ import React, { useState } from "react";
 import "./style.css";
 
 const Form = ({ addNewTask }) => {
-    const [newTaskContent, setNewTaskContent] = useState("");
+  const [newTaskContent, setNewTaskContent] = useState("");
 
-    const onFormSubmit = (event) => {
-        event.preventDefault();
-        
-        addNewTask(newTaskContent.trim());
-        setNewTaskContent("");
+  const onFormSubmit = (event) => {
+    event.preventDefault();
 
-        if (newTaskContent === "") {
-            return null;
-        }
+    if (newTaskContent.trim() === "") {
+      return;
     }
-    return (
-        <form className="form" onSubmit={onFormSubmit}>
-            <input
-                value={newTaskContent}
-                className="form__input"
-                type="text"
-                placeholder="Co jest do zrobienia?"
-                onChange={({ target }) => setNewTaskContent(target.value)}
-            />
-            <button className="form__button">Zatwierdż zadanie</button>
-        </form>
-    );
-}
-
-
-
-
+    addNewTask(newTaskContent.trim());
+    setNewTaskContent("");
+  };
+  return (
+    <form className="form" onSubmit={onFormSubmit}>
+      <input
+        value={newTaskContent}
+        className="form__input"
+        type="text"
+        placeholder="Co jest do zrobienia?"
+        onChange={({ target }) => setNewTaskContent(target.value)}
+      />
+      <button className="form__button">Zatwierdż zadanie</button>
+    </form>
+  );
+};
 
 export default Form;
