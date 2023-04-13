829a8b14861c0cb7afb17822a6f01d554757845b
Oskar Siry <oskarsiry99@gmail.com>
2023-04-13 13:22:39 +0200

    Style component added to Buttons

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,22 +1,18 @@
-import "./style.css"
+import { Div, Button } from "./styled.js";
 
 const Buttons = ({ tasks, hideDone, toggleHideDone, setAllDone }) => (
-    <div className="buttons">
-        {tasks.length > 0 && (
-            <>
-                <button onClick={toggleHideDone} className="buttons__style">
-                    {hideDone ? "Pokaż ukończone" : "Ukryj ukończone" }
-                </button>
-                <button
-                    onClick={setAllDone}
-                    className="buttons__style"
-                    disabled={tasks.every(({ done }) => done)}
-                >
-                    Ukończ wszystkie
-                </button>
-            </>
-        )}
-    </div>
+  <Div>
+    {tasks.length > 0 && (
+      <>
+        <Button onClick={toggleHideDone}>
+          {hideDone ? "Pokaż ukończone" : "Ukryj ukończone"}
+        </Button>
+        <Button onClick={setAllDone} disabled={tasks.every(({ done }) => done)}>
+          Ukończ wszystkie
+        </Button>
+      </>
+    )}
+  </Div>
 );
 
 export default Buttons;
---
+++ src/Buttons/styled.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const Div = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+export const Button = styled.button`
+  background-color: transparent;
+  border: none;
+  color: #15857eb0;
+  margin: 0 10px;
+  transition: color 0.5s;
+
+  @media (max-width: 1000px) {
+        flex-basis: 100%;
+        margin: 10px;
+    }
+ 
+ &:hover {
+    color: #0ed1c4b0;
+ }
+
+ &:disabled {
+    color: rgb(202, 210, 216);
+ }
+`;
