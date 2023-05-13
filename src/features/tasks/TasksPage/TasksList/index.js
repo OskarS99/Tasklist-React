b24e0772785634156cd1f5ac1761c63d29e1fa32
Oskar Siry <oskarsiry99@gmail.com>
2023-05-13 23:17:54 +0200

    useSelector corrected and location, params added

--- src/features/tasks/TasksPage/TasksList/index.js
+++ src/features/tasks/TasksPage/TasksList/index.js
@@ -8,32 +8,36 @@ import {
 import { useSelector, useDispatch } from "react-redux";
 import { useLocation } from "react-router-dom/cjs/react-router-dom";
 import { Link } from "react-router-dom/cjs/react-router-dom.min.js";
+import searchQueryParamName from "../searchQueryParamName.js";
 
 const TasksList = () => {
   const location = useLocation();
-  const query = new URLSearchParams(location.search).get("szukaj");
+  const query = new URLSearchParams(location.search).get(searchQueryParamName);
 
-  const  hideDone  = useSelector(selectHideDone);
-  const   tasks  = useSelector((state) => selectTasksByQuery(state, query));
+  const hideDone = useSelector(selectHideDone);
+  const tasks = useSelector((state) => selectTasksByQuery(state, query));
   const dispatch = useDispatch();
+    return (
+      <List>
+        {tasks.map((task) => (
+          <Item hidden={task.done && hideDone} key={task.id}>
+            <Button
+              toggleDone
+              onClick={() => dispatch(toggleTaskDone(task.id))}
+            >
+              {task.done ? "✔" : ""}
+            </Button>
+            <Content done={task.done}>
+              <Link to={`/zadania/${task.id}`}>{task.content}</Link>
+            </Content>
+            <Button Delete onClick={() => dispatch(removeTask(task.id))}>
+              🗑
+            </Button>
+          </Item>
+        ))}
+      </List>
+    );
 
-  return (
-    <List>
-      {tasks?.map((task) => (
-        <Item hidden={task.done && hideDone} key={task.id}>
-          <Button toggleDone onClick={() => dispatch(toggleTaskDone(task.id))}>
-            {task.done ? "✔" : ""}
-          </Button>
-          <Content done={task.done}>
-            <Link to={`/zadania/${task.id}`}>{task.content}</Link>
-          </Content>
-          <Button Delete onClick={() => dispatch(removeTask(task.id))}>
-            🗑
-          </Button>
-        </Item>
-      ))}
-    </List>
-  );
 };
 
 export default TasksList;
