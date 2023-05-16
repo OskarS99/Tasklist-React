a85de43c169d182e92008167a50dfc2bca9d3194
Oskar Siry <oskarsiry99@gmail.com>
2023-05-16 10:39:07 +0200

    folder changed for TaskPage component

---
+++ src/features/tasks/TaskPage/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Section from "../../../common/Section";
+import Header from "../../../common/Header";
+import Container from "../../../common/Container";
+import { GlobalStyle } from "../../../globalStyle";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectTaskById } from "../tasksSlice";
+
+const TaskPage = () => {
+  const { id } = useParams();
+  const task = useSelector((state) => selectTaskById(state, id));
+  return (
+    <Container>
+      <GlobalStyle />
+      <Header title="Szczegóły zadania" />
+      <Section
+        title={task ? task.content : "Nie znaleziono zadania 😢"}
+        body={
+          !!task && (
+            <>
+              <strong>Ukończono:</strong> {task.done ? "Tak" : "Nie"}{" "}
+            </>
+          )
+        }
+      />
+    </Container>
+  );
+};
+
+export default TaskPage;
