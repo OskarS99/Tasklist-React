566ce2b3cbd6eaaefc189078ee678804adf6d0f3
Oskar Siry <oskarsiry99@gmail.com>
2023-05-16 10:35:17 +0200

    code formatted

--- src/features/tasks/taskLocalStorage.js
+++ src/features/tasks/taskLocalStorage.js
@@ -3,12 +3,5 @@ export const localStorageKey = "tasks";
 export const saveTasksInLocalStorage = (tasks) =>
   localStorage.setItem(localStorageKey, JSON.stringify(tasks));
 
-  
-
 export const getTasksFromLocalStorage = () =>
   JSON.parse(localStorage.getItem(localStorageKey)) || [];
-   
-
- 
-
-  
