7dcb95af8105e7e161a2c267b45e03f1aecfa983
Oskar Siry <oskarsiry99@gmail.com>
2023-05-16 10:41:46 +0200

    code formatted

--- src/features/tasks/TasksPage/TasksList/index.js
+++ src/features/tasks/TasksPage/TasksList/index.js
@@ -6,8 +6,8 @@ import {
   selectHideDone,
 } from "../../tasksSlice.js";
 import { useSelector, useDispatch } from "react-redux";
-import { useLocation } from "react-router-dom/cjs/react-router-dom";
-import { Link } from "react-router-dom/cjs/react-router-dom.min.js";
+import { useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 import searchQueryParamName from "../searchQueryParamName.js";
 
 const TasksList = () => {
@@ -17,27 +17,23 @@ const TasksList = () => {
   const hideDone = useSelector(selectHideDone);
   const tasks = useSelector((state) => selectTasksByQuery(state, query));
   const dispatch = useDispatch();
-    return (
-      <List>
-        {tasks.map((task) => (
-          <Item hidden={task.done && hideDone} key={task.id}>
-            <Button
-              toggleDone
-              onClick={() => dispatch(toggleTaskDone(task.id))}
-            >
-              {task.done ? "✔" : ""}
-            </Button>
-            <Content done={task.done}>
-              <Link to={`/zadania/${task.id}`}>{task.content}</Link>
-            </Content>
-            <Button Delete onClick={() => dispatch(removeTask(task.id))}>
-              🗑
-            </Button>
-          </Item>
-        ))}
-      </List>
-    );
-
+  return (
+    <List>
+      {tasks.map((task) => (
+        <Item hidden={task.done && hideDone} key={task.id}>
+          <Button toggleDone onClick={() => dispatch(toggleTaskDone(task.id))}>
+            {task.done ? "✔" : ""}
+          </Button>
+          <Content done={task.done}>
+            <Link to={`/zadania/${task.id}`}>{task.content}</Link>
+          </Content>
+          <Button Delete onClick={() => dispatch(removeTask(task.id))}>
+            🗑
+          </Button>
+        </Item>
+      ))}
+    </List>
+  );
 };
 
 export default TasksList;
